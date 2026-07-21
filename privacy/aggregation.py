3ac563081a3e3d35b4f28bece80ec7eae2224a36
Akansha Nagar <akanshamaybe000@gmail.com>
2026-07-21 21:38:23 +0530

    Added skeleton structure for aggregation and communication

---
+++ privacy/aggregation.py
@@ -0,0 +1,11 @@
+from typing import List
+
+def receive_encrypted_updates():
+    pass
+
+def aggregate_encrypted_updates(encrypted_updates: List):
+    
+    pass
+def send_global_model(global_model):
+    
+    pass
