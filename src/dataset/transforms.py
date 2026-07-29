cb60b67fb754cc65bb9ce52e93978f334f372837
Shreya Ghuse <shreyaghuse@gmail.com>
2026-07-29 17:53:33 +0530

    Implemented MONAI preprocessing transforms

--- src/dataset/transforms.py
+++ src/dataset/transforms.py
@@ -2,24 +2,53 @@
 Image preprocessing transforms
 """
 
-from monai.transforms import Compose
+from monai.transforms import (
+    Compose,
+    LoadImaged,
+    EnsureChannelFirstd,
+    NormalizeIntensityd,
+    RandFlipd,
+    RandRotate90d,
+    ToTensord,
+)
 
 
 def get_train_transforms():
-    """
-    Returns preprocessing pipeline for training.
-    """
 
-    transforms = Compose([])
+    return Compose([
 
-    return transforms
+        LoadImaged(keys=["image", "mask"]),
+
+        EnsureChannelFirstd(keys=["image", "mask"]),
+
+        NormalizeIntensityd(keys="image"),
+
+        RandFlipd(
+            keys=["image", "mask"],
+            prob=0.5,
+            spatial_axis=0
+        ),
+
+        RandRotate90d(
+            keys=["image", "mask"],
+            prob=0.5
+        ),
+
+        ToTensord(keys=["image", "mask"])
+
+    ])
 
 
 def get_validation_transforms():
-    """
-    Returns preprocessing pipeline for validation.
-    """
 
-    transforms = Compose([])
+    return Compose([
 
-    return transforms
+        LoadImaged(keys=["image", "mask"]),
+
+        EnsureChannelFirstd(keys=["image", "mask"]),
+
+        NormalizeIntensityd(keys="image"),
+
+        ToTensord(keys=["image", "mask"])
+
+    ])
