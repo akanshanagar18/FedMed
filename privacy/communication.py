0fa2d10b7bf6467d9128ae1b62c6b94c1cca46b2
Akansha Nagar <akanshamaybe000@gmail.com>
2026-08-05 15:25:32 +0530

    Fix communication module and remove merge conflicts

--- privacy/communication.py
+++ privacy/communication.py
@@ -1,13 +1,36 @@
-<<<<<<< HEAD
+from typing import List
 
-=======
->>>>>>> e21892a (Update communication module)
+
+def receive_updates():
+    """
+    Receive encrypted model updates from clients.
+    """
+    pass
+
+
+def aggregate_encrypted(encrypted_updates: List):
+    """
+    Aggregate encrypted model updates.
+    """
+    pass
+
+
+def send_global_model(global_model):
+    """
+    Send encrypted global model to all clients.
+    """
+    pass
+
+
 def serialize_update(update):
-   
-def serialize_update(update):
+    """
+    Serialize encrypted update for transmission.
+    """
     pass
 
 
 def deserialize_update(data):
-  
+    """
+    Deserialize received encrypted update.
+    """
     pass
