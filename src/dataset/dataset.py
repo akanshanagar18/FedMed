c6d969b5bd3038a497cfc59bfb71e892fa453a7d
Shreya Ghuse <shreyaghuse@gmail.com>
2026-08-05 16:30:56 +0530

    implemented BraTS dataset loader

--- src/dataset/dataset.py
+++ src/dataset/dataset.py
@@ -1,7 +1,11 @@
 """
 BraTS Dataset Loader
+--------------------
+Loads MRI image paths and corresponding segmentation masks.
+Compatible with PyTorch DataLoader.
 """
 
+from pathlib import Path
 from torch.utils.data import Dataset
 
 
@@ -11,21 +15,35 @@ class BraTSDataset(Dataset):
     """
 
     def __init__(self, image_paths, mask_paths, transforms=None):
+        """
+        Args:
+            image_paths (list): List of MRI image file paths.
+            mask_paths (list): List of segmentation mask file paths.
+            transforms (callable): MONAI transforms.
+        """
+
         self.image_paths = image_paths
         self.mask_paths = mask_paths
         self.transforms = transforms
 
     def __len__(self):
+        """
+        Returns total number of samples.
+        """
+
         return len(self.image_paths)
 
     def __getitem__(self, index):
+        """
+        Returns one training sample.
+        """
 
         image_path = self.image_paths[index]
         mask_path = self.mask_paths[index]
 
         sample = {
-            "image": image_path,
-            "mask": mask_path
+            "image": str(image_path),
+            "mask": str(mask_path)
         }
 
         if self.transforms:
